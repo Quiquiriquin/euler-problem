d873c92732c8e3f6fb995193cb4125ae06a461c0
Enrique Alvarez <alvarez.enrique0508@gmail.com>
2019-05-31 00:00:57 -0500

    Enable errors

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,12 @@ app.use(cors())
 app.use('/api/v1', routes)
 
 
-catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-error handler
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
